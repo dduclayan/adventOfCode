3f8665405f513d7f13f1529cea9e2ee52329f635
dduclayan <dduclayan@gmail.com>
2022-12-11 05:19:31 -0600

    Update template.

--- 2022/template/main.go
+++ 2022/template/main.go
@@ -20,35 +20,24 @@ func init() {
 }
 
 func main() {
-	//start := time.Now()
-	//dir, err := os.Getwd()
-	//if err != nil {
+	// start := time.Now()
+	// dir, err := os.Getwd()
+	// if err != nil {
 	//	logger.Fatalf("couldn't get working dir: %v", err)
-	//}
-	//splitDir := strings.Split(dir, "\\")
-	//day := splitDir[6]
-	//actualInputPath := fmt.Sprintf(testDataPathTemplate, day)
-	//smallInputPath := fmt.Sprintf(smallDataTemplate, day)
-	//a, err := answerOne(actualInputPath)
-	//if err != nil {
-	//	fmt.Printf("couldn't retrieve answer: %v\n", err)
-	//}
-	//
-	//b, err := answerTwo(actualInputPath)
-	//if err != nil {
-	//	fmt.Printf("couldn't retrieve answer: %v\n", err)
-	//}
-	//fmt.Printf("part one answer: %v\n", a)
-	//fmt.Printf("part two answer: %v\n", b)
-	//fmt.Printf("finished executing in %v\n", time.Since(start))
+	// }
+	// splitDir := strings.Split(dir, "\\")
+	// day := splitDir[6]
+	// actualInputPath := fmt.Sprintf(testDataPathTemplate, day)
+	// smallInputPath := fmt.Sprintf(smallDataTemplate, day)
+	// answerOne(smallInputPath)
+	// answerTwo(smallInputPath)
+	// fmt.Printf("part one answer: %v\n", a)
+	// fmt.Printf("part two answer: %v\n", b)
+	// fmt.Printf("finished executing in %v\n", time.Since(start))
 }
 
-//
-//func answerOne(filePath string) error {
-//	return nil
-//}
-//
-//func answerTwo(filePath string) error {
-//
-//	return nil
-//}
+// func answerOne(filePath string) {
+// }
+
+// func answerTwo(filePath string) {
+// }
